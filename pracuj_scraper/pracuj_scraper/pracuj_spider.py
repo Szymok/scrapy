ff25dd9473f86b452ce69f910dab9456537f3bcc
Szymok <szymonkowal019@gmail.com>
2024-04-02 23:11:37 +0200

    edit pracuj scraper to select script info

--- pracuj_scraper/pracuj_scraper/pracuj_spider.py
+++ pracuj_scraper/pracuj_scraper/pracuj_spider.py
@@ -1,36 +1,39 @@
 import scrapy
-from pracuj_scraper.items import PracujScraperItem
+import json
 
 class PracujSpider(scrapy.Spider):
     name = 'pracuj_spider'
     allowed_domains = ['pracuj.pl']
     start_urls = ['https://www.pracuj.pl/praca/ksiegowy;kw/gdynia;wp?rd=30']
 
-    def __init__(self, *args, **kwargs):
-        super(PracujSpider, self).__init__(*args, **kwargs)
-        self.page_number = 1  # Initialize the page counter
-
     def parse(self, response):
-        # Use the correct class selector with periods and no spaces
-        for job in response.css('.tiles_b4e7s16.core_po9665q'):
-            item = PracujScraperItem()
-            # Uncomment and update the selectors below as needed
-            # item['salary'] = job.css('li.offer__info--salary::text').get()
-            # item['company'] = job.css('a.offer-details__company-name::text').get()
-            # item['url'] = response.urljoin(job.css('a.offer-details__title-link::attr(href)').get())
-            # item['localization'] = job.css('li.offer-labels__item--location::text').get()
-            # item['date'] = job.css('span.offer-actions__date::text').get()
-            
-            # Update the selector for the title to match the correct class or structure
-            item['title'] = job.css('h2::text').get()  # Assuming the title is within an <h2> tag
-            
-            yield item
+        # Extract the JSON data from the <script> tag
+        script_data = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
+        
+        # Parse the JSON data
+        json_data = json.loads(script_data)
+        
+        # Extract the job listings from the JSON data
+        job_listings = json_data['props']['pageProps']['data']['jobOffers']['groupedOffers']
+        
+        for job in job_listings:
+            yield {
+                'title': job['jobTitle'],
+                'company': job['companyName'],
+                'lastPublicated': job.get('lastPublicated', ''),
+                'expirationDate': job.get('expirationDate', ''),
+                'salaryDisplayText': job.get('salaryDisplayText', ''),
+                'jobDescription': job.get('jobDescription', ''),
+                'offerAbsoluteUri': job.get('offerAbsoluteUri', ''),
+                'displayWorkplace': job.get('displayWorkplace', ''),
+                'positionLevels': job.get('positionLevels', []),
+                'typesOfContract': job.get('typesOfContract', []),
+                'workSchedules': job.get('workSchedules', []),
+                'workModes': job.get('workModes', []),
+                'primaryAttributes': job.get('primaryAttributes', [])
+            }
 
-        # Increment the page number
-        self.page_number += 1
-        next_page_url = f'https://www.pracuj.pl/praca/ksiegowy;kw/gdynia;wp?rd=30&pn={self.page_number}'
-
-        # Check if there is a next page by looking for the presence of job offers on the page
-        # This is a basic check and might need to be adjusted based on the actual page structure
-        if response.css('.tiles_b4e7s16.core_po9665q'):
-            yield response.follow(next_page_url, self.parse)
+        # Check if there is a next page
+        next_page_url = response.css('a.pagination_bfwjnw0[data-test="link-pagination-next"]::attr(href)').get()
+        if next_page_url:
+            yield scrapy.Request(url=response.urljoin(next_page_url), callback=self.parse)
